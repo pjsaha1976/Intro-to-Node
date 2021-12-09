a5cef6e025c67f85d479ec8acac9bb574c4c2343
PJ Saha <pjsaha1976@gmail.com>
2021-12-08 18:30:43 -0800

    updated mynode.js

--- mynode.js
+++ mynode.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-const PORT = 8080; //process.env.PORT || 5000
+const PORT = process.env.PORT || 5000; //8080
 
 http.createServer(function(req, res) {
     var q = url.parse(req.url, true);
